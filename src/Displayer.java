06bbd57b3e09b5b50e12ea3478fb9a1f7013216d
Manuel Braunwald <manuelbrusche@gmail.com>
2024-06-22 18:37:02 +0200

    -refactor

--- src/Displayer.java
+++ src/Displayer.java
@@ -1,17 +1,62 @@
 import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import static BraunAssertions.BraunAssert.bassert;
 
 public class Displayer {
     private String currentText;
-    
-    public Displayer(String totalText) {
+    private JTextField textField;
+
+    public Displayer(String totalText, JLabel label) {
         setCurrentText(totalText);
-    }
+        label.setText(totalText);
         
+        if (this.textField == null) {
+            this.textField = new JTextField();
+        }
+        
+        setTextFieldListeners(label);
+    }
+
+    private void setTextFieldListeners(JLabel label) {
+        this.textField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) { }
+
+            @Override
+            public void keyPressed(KeyEvent e) { }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                    // trim text in label if word is right
+                    String typedWord = textField.getText();
+                    bassert(typedWord.endsWith(" "), "no space found");
+                    typedWord = typedWord.substring(0,typedWord.length()-1);
+                    bassert(!typedWord.endsWith(" "), "space not removed");
+
+                    if (typedWord.equals(getCurrentWord())) {
+                        //trim currenttext by that word
+                        trimFirstWord();
+
+                        //TODO handle last word
+
+                        //update the label
+                        label.setText(getCurrentText());
+
+                        //reset textfield
+                        textField.setText(null);
+                    }
+
+                }
+            }
+        });
+    }
+
     private void setCurrentText(String text) {
        this.currentText = text; 
     }
-
-    public static void test(String a, int b, InputMap c){}
 
     public String getCurrentText() {
         return this.currentText;
@@ -25,5 +70,12 @@ public class Displayer {
         String replace = this.currentText.replaceFirst(
                 String.format("%s ", this.getCurrentWord()), "");
         this.currentText = replace;
+    }
+
+    public JTextField getTextField() {
+        return this.textField;
+    }
+    public void setTextField(JTextField textField) {
+        this.textField = textField;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -15,50 +15,13 @@ public class Main {
         frame.setMaximumSize(new Dimension(800, 600));
         frame.setLayout(new GridLayout(2, 1));
         
-        var displayer = new Displayer(totalText);
-        var label = new JLabel(displayer.getCurrentText());
+        var label = new JLabel();
         label.setSize(200,100);
         frame.add(label);
-        
-        var textField = new JTextField();
-        textField.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
 
-            }
 
-            @Override
-            public void keyPressed(KeyEvent e) {
-               
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-                    // trim text in label if word is right
-                    String typedWord = textField.getText();
-                    bassert(typedWord.endsWith(" "), "no space found");
-                    typedWord = typedWord.substring(0,typedWord.length()-1);
-                    bassert(!typedWord.endsWith(" "), "space not removed");
-                    
-                    if (typedWord.equals(displayer.getCurrentWord())) {
-                        //trim currenttext by that word
-                        displayer.trimFirstWord();
-
-                        //TODO handle last word
-                        
-                        //update the label
-                        label.setText(displayer.getCurrentText());
-
-                        //reset textfield
-                        textField.setText(null);
-                    }
-
-                }
-            }
-        });
-        
-        frame.add(textField);
+        var displayer = new Displayer(totalText, label);
+        frame.add(displayer.getTextField());
         
         frame.setVisible(true);
         
